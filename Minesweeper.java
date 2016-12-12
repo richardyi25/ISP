f0726dc294c5424e277554e26bcbe2a61203cd64
Richard Yi <richardyi25@gmail.com>
2016-12-12 11:27:32 -0500

    Monday during History

--- Minesweeper.java
+++ Minesweeper.java
@@ -11,6 +11,7 @@ public class Minesweeper
     int grid[] [] = new int [25] [25];
     int size;
     char menuChoice;
+    Font titleFont = new Font ("Comic Sans MS", 0, 40);
 
 
     public Minesweeper ()
@@ -21,21 +22,11 @@ public class Minesweeper
     }
 
 
-    public void mainMenu ()
+    private void pauseProgram (int y)
     {
-	c.clear ();
-
-	c.setFont (new Font ("Comic Sans MS", 0, 40));
-	c.drawString ("Minesweeper", 285, 50);
-
-	c.setFont (new Font ("Comic Sans MS", 0, 20));
-
-	c.drawString ("Press 1 to play easy mode", 10, 100);
-	c.drawString ("Press 1 to play easy mode", 10, 150);
-	c.drawString ("Press 1 to play easy mode", 10, 200);
-	c.drawString ("Press 1 to play easy mode", 10, 250);
-	c.drawString ("Press 1 to play easy mode", 10, 300);
-	c.drawString ("Press 1 to play easy mode", 10, 350);
+	c.setFont (new Font ("Comic Sans MS", 0, 30));
+	c.drawString ("Press any key to continue...", 50, y);
+	c.getChar ();
     }
 
 
@@ -44,31 +35,100 @@ public class Minesweeper
 	SplashScreen s = new SplashScreen (c);
 
 	s.start ();
+    }
 
 
-	public static void main (String[] args) throws IOException
+    public void mainMenu ()
+    {
+	c.clear ();
+
+	c.setFont (titleFont);
+	c.drawString ("Minesweeper", 285, 50);
+
+	c.setFont (new Font ("Comic Sans MS", 0, 20));
+
+	c.drawString ("Press 1 to play easy mode", 10, 100);
+	c.drawString ("Press 2 to play medium mode", 10, 150);
+	c.drawString ("Press 3 to play hard mode", 10, 200);
+	c.drawString ("Press Q for help and instructions", 10, 250);
+	c.drawString ("Press W to see high scores", 10, 300);
+	c.drawString ("Press E to quit", 10, 350);
+	//change to button format if you have time
+
+	while (true)
 	{
-	    Minesweeper m = new Minesweeper ();
+	    menuChoice = c.getChar ();
 
-
-	    m.mainMenu ();
-	    /*
-		    while (true)
-		    {
-			m.mainMenu ();
-
-			if (m.menuChoice == '1' || m.menuChoice == '2' || m.menuChoice == '3')
-			    m.game ();
-			else if (m.menuChoice == 'Q' || m.menuChoice == 'q')
-			    m.instructions ();
-			else if (m.menuChoice == 'W' || m.menuChoice == 'w')
-			    m.highScores ();
-			else if (m.menuChoice == 'E' || m.menuChoice == 'e')
-			    break;
-		    }
-	    */
-
-	    //m.goodbye ();
+	    switch (menuChoice)
+	    {
+		case '1':
+		case '2':
+		case '3':
+		case 'q':
+		case 'w':
+		case 'e':
+		case 'Q':
+		case 'W':
+		case 'E':
+		    return;
+	    }
 	}
     }
+
+
+    public void instructions ()
+    {
+	c.clear ();
+
+	c.setFont (titleFont);
+	c.drawString ("Instructions", 285, 50);
+
+	c.getChar ();
+    }
+
+
+    public void highScores ()
+    {
+
+    }
+
+
+    public void goodbye ()
+    {
+	c.setFont (new Font ("Comic Sans MS", 0, 30));
+	c.drawString ("Thank you for using my program", 69, 300);
+	c.drawString ("Made by: Richard Yi", 69, 400);
+
+	pauseProgram (500);
+	c.close ();
+    }
+
+
+    public void game ()
+    {
+
+    }
+
+
+    public static void main (String[] args) throws IOException
+    {
+	Minesweeper m = new Minesweeper ();
+
+	while (true)
+	{
+	    m.mainMenu ();
+
+	    if (m.menuChoice == '1' || m.menuChoice == '2' || m.menuChoice == '3')
+		m.game ();
+	    else if (m.menuChoice == 'Q' || m.menuChoice == 'q')
+		m.instructions ();
+	    else if (m.menuChoice == 'W' || m.menuChoice == 'w')
+		m.highScores ();
+	    else if (m.menuChoice == 'E' || m.menuChoice == 'e')
+		break;
+	}
+
+	m.goodbye ();
+    }
 }
+
